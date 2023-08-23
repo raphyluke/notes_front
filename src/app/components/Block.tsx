125aa7096abd6655e7afcbf62fb6bc144f5ef20d
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-08-23 00:47:31 +0000

    Remove the new line of textarea on shift + enter

--- src/app/components/Block.tsx
+++ src/app/components/Block.tsx
@@ -66,6 +66,7 @@ export default function Block({data} : any){
         }
         // if enter and shift at the same time
         else if (e.key === "Enter" && e.shiftKey){
+          e.preventDefault()
           dispatch(addBlock({
             id : uuidv4(),
             order : data.order + 1,
@@ -204,7 +205,7 @@ export default function Block({data} : any){
             <FontAwesomeIcon icon={faGripVertical} className='m-2' width={10} color='grey' onClick={(e) => setBoxIcon(!boxIcon)} />
           </div> : <div className='w-14 flex'></div>}
           {boxIcon ? <div className=' absolute left-0 z-10 flex'><BoxIcons setBoxIcon={setBoxIcon} data={data} /><div className=' bg-white w-5 h-5 flex justify-center items-center' onClick={(e) => setBoxIcon(false)}>x</div></div> : <div></div>}
-          {isOver ? <textarea onInput={(e) => textAreaInputRule(e)} value={data.content} onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} ref={inputRef} className='overflow-hidden resize-none bg-transparent w-full ml-16 text-lg' placeholder='Add a task...' /> : <textarea value={data.content} onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} ref={inputRef} onInput={(e) => textAreaInputRule(e)} className='overflow-hidden resize-none bg-transparent w-full focus:outline-none text-lg ml-2.5' placeholder='Add a task...' />}
+          {isOver ? <textarea rows={1} onInput={(e) => textAreaInputRule(e)} value={data.content} onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} ref={inputRef} className='overflow-hidden resize-none bg-transparent w-full ml-16 text-lg' placeholder='Add a task...' /> : <textarea value={data.content} onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} ref={inputRef} onInput={(e) => textAreaInputRule(e)} rows={1} className='overflow-hidden resize-none bg-transparent w-full focus:outline-none text-lg ml-2.5 h-fit' placeholder='Add a task...' />}
         </div>
       )
     }
