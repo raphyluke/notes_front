1dc342682f6595838b1c722057e134de92a01081
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-08-27 21:13:36 +0000

    Begin drag and drop

--- src/app/app/notes/[slug]/page.tsx
+++ src/app/app/notes/[slug]/page.tsx
@@ -66,6 +66,23 @@ export default function Page({params} : {params : {slug : string}}) {
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error</div>
 
+  // Drag and drop functionality
+  function handleDragStart(e: any, index: number) {
+    e.dataTransfer.setData('index', index.toString());
+  }
+
+  function handleDragOver(e: any) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e: any, newIndex: number) {
+    const oldIndex = parseInt(e.dataTransfer.getData('index'));
+    const newBlocks = [...blocks];
+    const [removed] = newBlocks.splice(oldIndex, 1);
+    newBlocks.splice(newIndex, 0, removed);
+    setBlocks(newBlocks);
+  }
+
   return (
     <main className="h-full w-9/12">
       <div className="flex ml-16 w-full items-center mt-2">
@@ -74,7 +91,11 @@ export default function Page({params} : {params : {slug : string}}) {
         {blocks && <p className="m-3">{noteNameRef.current?.value}</p>}
       </div>
       <input ref={noteNameRef} onChange={(e) => onChangeName(e)} className='w-full focus:outline-none ml-20 mt-6 font-semibold text-xl mb-8' />
-      {blocks && blocks.map((block: any, index : any) => (<div key={block.id}><Block data={block} index={index} /></div>))}
+      {blocks && blocks.map((block: any, index : any) => (<div key={block.id}
+          draggable
+          onDragStart={(e) => handleDragStart(e, index)}
+          onDragOver={handleDragOver}
+          onDrop={(e) => handleDrop(e, index)}><Block data={block} index={index} /></div>))}
     </main>
   )
 }
