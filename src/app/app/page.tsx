04a2e7e8c6151274af6a744b0954249c70525f65
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2023-10-17 04:00:33 +0000

    remove home page + setup menu sidebar

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -14,13 +14,7 @@ export default function Home() {
 
   return (
     <main className="h-full w-9/12 ml-72">
-      <div className="flex ml-16 w-full items-center mt-2">
-        <FontAwesomeIcon icon={faHome} className="m-3" width={24} color='grey' />
-        <FontAwesomeIcon icon={faChevronRight} className="m-3" width={10} color='grey' />
-        <p className="m-3">Coucou</p>
-        <FontAwesomeIcon icon={faChevronRight} className="m-3" width={10} color='grey' />
-        <p className="m-3">Coucou</p>
-      </div>
+      
     </main>
   )
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dropdown from './Dropdown'
 import { useSelector, useDispatch } from 'react-redux';
 import { createNotes } from '../redux/thunks/createNotes';
@@ -7,12 +7,15 @@ import { getAllNotes } from '../redux/thunks/getAllNotes';
 import { store } from '../redux/store';
 import jwtDecode from 'jwt-decode';
 import { setUser } from '../redux/slices/userSlices';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar(){
     // Redux hooks
     const user = useSelector((state: any) => state.user.user)
     const notes = useSelector((state: any) => state.notes.notes)
     const dispatch = useDispatch<typeof store.dispatch>()
+    const [menu, setMenu] = useState(false)
 
     // Get all notes and set the user
     useEffect(() => {
@@ -33,14 +36,25 @@ export default function Sidebar(){
       dispatch(createNotes())
     }
 
+    function disconnect(){
+      localStorage.removeItem('token')
+      window.location.href = 'http://localhost:5173/login'
+    }
+
     return (
         <aside className='flex flex-col w-80 h-full fixed shadow-sm shadow-black'>
-          <div className='flex items-center m-5 justify-between'>
+          <div className='flex items-center m-5 justify-between relative'>
             <div className='flex items-center'>
               <img src={user.picture} alt={user.email} className='h-6 w-6 bg-slate-600'/>
               <h1 className='ml-3'>{user.first_name} {user.last_name}</h1>
             </div>
-            <div className='h-4 w-4 bg-slate-500 rounded-xl'></div>
+            <div className='h-4 w-4 bg-slate-500 rounded-xl' onClick={(e) => setMenu(!menu)}></div>
+            {menu && <div className='w-32 bg-white absolute right-5 top-0 border-slate-400 border rounded-xl shadow-2xl'>
+              <ul className='pl-2 flex flex-col'>
+                <li className='text-black py-1'><FontAwesomeIcon icon={faCog} /> Settings</li>
+                <li className='text-red-500 py-1' onClick={(e) => disconnect()}><FontAwesomeIcon icon={faSignOut} /> Logout</li>
+              </ul>
+              </div>}
           </div>
           <button className='bg-[#5167F6] m-5 h-10 font-bold text-white' onClick={(e) => createNote()}>
             Create
